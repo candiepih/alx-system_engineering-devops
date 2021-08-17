df5c02d5af9ab9e197c72a7b2f209a5eb03ef7fd
candiepih <mutheeal.am@gmail.com>
2021-08-17 20:10:55 +0000

    added puppet manifest file

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,34 @@
+# configure nginx server to listen to port 80
+# with permanent redirect for /redirect_me and handle
+# custom error 404 page
+
+package {'nginx':
+  ensure => installed,
+}
+
+file {'/var/www/html/index.html':
+  ensure  => present
+  content => 'Holberton School',
+}
+
+file_line {'server_name _;':
+  path  => '/etc/nginx/sites-available/default',
+  line  => "\n\tlocation /redirect_me {\n\t\trewrite ^/redirect_me(.*)$ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;\n\t}",
+  after => 'server_name _;'
+}
+
+file {'/var/www/html/404.html':
+  ensure  => present
+  content => "Ceci n'est pas une page",
+}
+
+file_line {'server_name _;':
+  path  => '/etc/nginx/sites-enabled/default',
+  line  => "\n\t error_page 404 /404.html;\n\tlocation = /404.html {\n\t\troot /var/www/html;\n\t\tinternal;\n\t}",
+  after => 'server_name _;'
+}
+
+service {'nginx':
+  ensure  => running,
+  require => Package['nginx']
+}
