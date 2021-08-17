061cd5c309334d40e7190bf9cddfa53b899cb1c6
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-08-17 23:33:58 +0300

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -7,21 +7,36 @@ exec {'apt-get-update':
 }
 
 package {'nginx':
-  ensure => installed,
+  ensure => 'installed',
 }
 
 file {'/var/www/html/index.html':
-  ensure  => present,
+  ensure  => 'present',
   content => 'Holberton School',
+  require => Package['nginx']
 }
 
 file_line {'server_name _;':
   path  => '/etc/nginx/sites-available/default',
   line  => "\n\tlocation /redirect_me {\n\t\trewrite ^/redirect_me(.*)$ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;\n\t}",
-  after => 'server_name _;'
+  after => 'server_name _;',
+  require => Package['nginx']
+}
+
+file {'/var/www/html/404.html':
+  ensure  => 'present',
+  content => "Ceci n'est pas une page",
+  require => Package['nginx']
+}
+
+file_line {'server_name _; ':
+  path  => '/etc/nginx/sites-enabled/default',
+  line  => "\n\terror_page 404 /404.html;\n\tlocation = /404.html {\n\t\troot /var/www/html;\n\t\tinternal;\n\t}",
+  after => 'server_name _;',
+  require => Package['nginx']
 }
 
 service {'nginx':
-  ensure  => running,
+  ensure  => 'running',
   require => Package['nginx']
 }
