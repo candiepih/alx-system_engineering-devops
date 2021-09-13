45fb5aa578c9cc55242d5e3caf9724f1e652eb71
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-09-13 14:33:03 +0300

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -7,27 +7,28 @@ import requests
 from sys import argv
 
 
-employee_id = argv[1]
-user_url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
-todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos" \
-    .format(employee_id)
-
-user_response = requests.get(user_url)
-todos_response = requests.get(todos_url)
-
-try:
-    user = user_response.json()
-    user_todos = todos_response.json()
-    csv_name = "{}.csv".format(user.get("id"))
-    with open(csv_name, mode="w", encoding="utf-8", newline='') as data_file:
-        csv_writer = csv.writer(data_file)
-        for todo in user_todos:
-            row = [
-                user.get("id"),
-                user.get("username"),
-                todo.get("completed"),
-                todo.get("title")
-            ]
-            csv_writer.writerow(row)
-except Exception as e:
-    print(e)
+if __name__ == "__main__":
+    employee_id = argv[1]
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
+    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos" \
+        .format(employee_id)
+    
+    user_response = requests.get(user_url)
+    todos_response = requests.get(todos_url)
+    
+    try:
+        user = user_response.json()
+        user_todos = todos_response.json()
+        csv_name = "{}.csv".format(user.get("id"))
+        with open(csv_name, mode="w", encoding="utf-8", newline='') as data_file:
+            csv_writer = csv.writer(data_file)
+            for todo in user_todos:
+                row = [
+                    user.get("id"),
+                    user.get("username"),
+                    todo.get("completed"),
+                    todo.get("title")
+                ]
+                csv_writer.writerow(row)
+    except Exception as e:
+        print(e)
