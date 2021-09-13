007d484921acd7112548a72b6e524864472aaceb
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-09-13 14:31:25 +0300

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,24 +7,25 @@ import requests
 from sys import argv
 
 
-employee_id = argv[1]
-user_url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
-todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos" \
-    .format(employee_id)
-
-user_response = requests.get(user_url)
-todos_response = requests.get(todos_url)
-
-try:
-    user = user_response.json()
-    user_todos = todos_response.json()
-    completed_tasks = list(filter(lambda x: x.get("completed") is True,
-                                  user_todos))
-    print("Employee {} is done with tasks({}/{}):"
-          .format(user.get("name"), len(completed_tasks),
-                  len(user_todos)))
-
-    for todo in completed_tasks:
-        print("\t{}".format(todo.get("title")))
-except Exception as e:
-    print(e)
+if __name__ == "__main__":
+    employee_id = argv[1]
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
+    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos" \
+        .format(employee_id)
+    
+    user_response = requests.get(user_url)
+    todos_response = requests.get(todos_url)
+    
+    try:
+        user = user_response.json()
+        user_todos = todos_response.json()
+        completed_tasks = list(filter(lambda x: x.get("completed") is True,
+                                      user_todos))
+        print("Employee {} is done with tasks({}/{}):"
+              .format(user.get("name"), len(completed_tasks),
+                      len(user_todos)))
+    
+        for todo in completed_tasks:
+            print("\t{}".format(todo.get("title")))
+    except Exception as e:
+        print(e)
