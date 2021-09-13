83eec9a45e22c2c7730cce90c7e827447f7750a6
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-09-13 14:34:36 +0300

    Update 3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -6,32 +6,33 @@ import json
 import requests
 
 
-user_url = "https://jsonplaceholder.typicode.com/users"
-todos_url = "https://jsonplaceholder.typicode.com/todos"
+if __name__ == "__main__":
+    user_url = "https://jsonplaceholder.typicode.com/users"
+    todos_url = "https://jsonplaceholder.typicode.com/todos"
 
-user_response = requests.get(user_url)
-todos_response = requests.get(todos_url)
+    user_response = requests.get(user_url)
+    todos_response = requests.get(todos_url)
 
-try:
-    users = user_response.json()
-    todos = todos_response.json()
+    try:
+        users = user_response.json()
+        todos = todos_response.json()
 
-    data = {}
-    for user in users:
-        user_id = user.get("id")
-        user_todos = list(filter(lambda todo: (todo.get("userId") == user_id),
-                                 todos))
-        tasks = list(map(lambda todo: {
-                             "username": user.get("username"),
-                             "task": todo.get("title"),
-                             "completed": todo.get("completed"),
-                         },
-                         user_todos))
-        data[user_id] = tasks
+        data = {}
+        for user in users:
+            user_id = user.get("id")
+            user_todos = list(filter(lambda todo: (todo.get("userId") == user_id),
+                                     todos))
+            tasks = list(map(lambda todo: {
+                                 "username": user.get("username"),
+                                 "task": todo.get("title"),
+                                 "completed": todo.get("completed"),
+                             },
+                             user_todos))
+            data[user_id] = tasks
 
-    json_file = "todo_all_employees.json"
-    with open(json_file, mode="w", encoding="utf-8") as f:
-        json.dump(data, f)
+        json_file = "todo_all_employees.json"
+        with open(json_file, mode="w", encoding="utf-8") as f:
+            json.dump(data, f)
 
-except Exception as e:
-    print(e)
+    except Exception as e:
+        print(e)
