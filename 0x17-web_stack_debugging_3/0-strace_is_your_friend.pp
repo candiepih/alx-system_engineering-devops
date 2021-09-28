171dd782123f5d1c6c082fc1bda86b8697eb21d9
candiepih <mutheeal.am@gmail.com>
2021-09-28 13:30:51 +0000

    initial commit

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# Editing wordpress setting file typo causing
+# server error 500
+
+exec {'Fixing typos':
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
+}
