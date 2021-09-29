47b0d7ecd8c1179f8ef32a9fe6dc506c2099e2c1
candiepih <mutheeal.am@gmail.com>
2021-09-29 13:49:42 +0000

    formatted file to puppet-lint linting

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -3,5 +3,5 @@
 
 exec {'Fixing typos':
   command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
-  path => '/usr/bin/:/usr/local/bin/:/bin/'
+  path    => '/usr/bin/:/usr/local/bin/:/bin/'
 }
